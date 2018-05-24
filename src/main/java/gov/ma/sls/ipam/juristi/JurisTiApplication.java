7f97f59a5f23ea2f97f7c7af6237bc4f7ea8e21d
Simony Cantanhede <simonycanunes@gmail.com>
2018-05-24 10:15:05 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/gov/ma/sls/ipam/juristi/JurisTiApplication.java
@@ -0,0 +1,12 @@
+package gov.ma.sls.ipam.juristi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurisTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurisTiApplication.class, args);
+	}
+}
